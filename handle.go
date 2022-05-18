5a66c973c6926e3c2bc99ecee207ef6a3cad53a3
salaboy <Salaboy@gmail.com>
2022-05-18 08:53:11 +0100

    adding redis tls

--- handle.go
+++ handle.go
@@ -2,6 +2,7 @@ package function
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
@@ -12,6 +13,8 @@ import (
 
 var redisHost = os.Getenv("REDIS_HOST") // This should include the port which is most of the time 6379
 var redisPassword = os.Getenv("REDIS_PASSWORD")
+var redisTLSEnabled = os.Getenv("REDIS_TLS")
+var redisTLSEnabledFlag = false
 
 type GameSession struct{
 	SessionId string
@@ -21,11 +24,27 @@ type GameSession struct{
 // Handle an HTTP Request.
 func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 
-	client := redis.NewClient(&redis.Options{
-		Addr:     redisHost,
-		Password: redisPassword,
-		DB:       0,
-	})
+	if redisTLSEnabled != "" && redisTLSEnabled != "false" {
+		redisTLSEnabledFlag = true
+	}
+	var client *redis.Client
+
+	if !redisTLSEnabledFlag {
+		client = redis.NewClient(&redis.Options{
+			Addr:     redisHost,
+			Password: redisPassword,
+			DB:       0,
+		})
+	} else {
+		client = redis.NewClient(&redis.Options{
+			Addr:     redisHost,
+			Password: redisPassword,
+			DB:       0,
+			TLSConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+			},
+		})
+	}
 
 	nickname, _ := req.URL.Query()["nickname"]
 	var g GameSession
